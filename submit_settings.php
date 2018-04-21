5fcaea500338f0109193d037a67d009a7d28ea09
andreaspapav <32335560+andreaspapav@users.noreply.github.com>
2018-04-21 18:10:08 +0100

    Add files via upload

--- submit_settings.php
+++ submit_settings.php
@@ -7,5 +7,5 @@ file_put_contents('settings.json', json_encode([
   
 ]));
 
-header( 'Location: http://localhost:8888/OrangeDayPrototype2/settings.php' ) ;
+header( 'Location: http://localhost:8888/OrangeDayFinished/settings.php' ) ;
 ?>
